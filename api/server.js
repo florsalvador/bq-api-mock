5069ed3977100024b927c951589def3969c0d9a1
florsalvador <florsalvador219@gmail.com>
2024-09-16 00:24:35 -0500

    Modify server.js

--- api/server.js
+++ api/server.js
@@ -5,14 +5,13 @@ const auth = require('json-server-auth');
 const middlewares = jsonServer.defaults()
 
 const app = jsonServer.create();
+const dbFile = path.join(__dirname, 'db.json');
+const dbData = JSON.parse(fs.readFileSync(dbFile, 'utf-8'));
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
 const port = process.env.PORT || 8080;
 
 const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
-
-// /!\ Bind the router db to the app
-app.db = router.db
 
 // You must apply the auth middleware before the router
 app.use(middlewares);
